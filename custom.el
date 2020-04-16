427d61fa2633c4fd1e1debb44bdf61a2d16cc68e
Jeldo Arno Meppen <jameppen@mailbox.org>
2020-04-16 20:21:48 +0200

    Initial commit

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("855eb24c0ea67e3b64d5d07730b96908bac6f4cd1e5a5986493cbac45e9d9636" default)))
+ '(package-selected-packages
+   (quote
+    (hydra page-break-lines ergoemacs-mode evil-org evil-indent-textobject evil-surround evil-leader evil ivy-posframe centaur-tabs dashboard neotree olivetti org-superstar org-superstar-mode org-fragtog magit which-key ledger-mode counsel swiper ivy company org-plus-contrib switch-window dmenu exwm smart-mode-line-atom-one-dark-theme atom-one-dark-theme pretty-mode async undo-tree use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#282C34" :foreground "#ABB2BF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "PfEd" :family "Fantasque Sans Mono"))))
+ '(org-document-title ((t (:weight bold :height 1.5))))
+ '(org-ellipsis ((t (:foreground "LightGoldenrod")))))
---
+++ init.el
@@ -0,0 +1,19 @@
+;;; Offload the custom-set-variables to a separate file
+;;; This keeps your init.el neater and you have the option
+;;; to gitignore your custom.el if you see fit.
+(setq custom-file "~/.emacs.d/custom.el")
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+;;; Load custom file. Don't hide errors. Hide success message.
+(load custom-file nil t)
+
+;;; Setup package.el
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(unless package--initialized (package-initialize))
+
+;;; Load config.org
+;;; All of my configuration will happen there, so don't you touch this file!
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
